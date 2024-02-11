8d2bf2bb26bf4e0423a8e709ee45db926542cfda
Omar Gonzalez <om.gonzalez007@gmail.com>
2024-02-10 20:46:53 -0500

    improve code

--- nth-prime.py
+++ nth-prime.py
@@ -12,16 +12,18 @@ def prime_nth(n):
                   primes_list.append(number)
             number += 1
             flag = 0
+
+
 try:
-    print("Given a number n, determine what the nth prime is.")
-    n =  int(input("Enter a number: "))
-    if n < 1: 
-      print(f"Error, you typed a negative number {n}, you must to type a number > 0")
-    elif n == 0:
-        print(f"Error, you typed {n}, you must to type a number > 0")
-    else:
-        nthPrime = prime_nth(n)
-        print(f"The {n}th number prime is {nthPrime}")
+        
+        print("Given a number n, determine what the nth prime is.")
+        n =  int(input("Enter a number: "))
+        if n < 1: 
+            print(f"Error, you typed {n}, you must to type a number > 0")
+        else:
+            nthPrime = prime_nth(n)
+            print(f"The {n}th number prime is {nthPrime}")
+        
 except Exception as e:
      print("Error, you typed a string, you must to type a number!")
 
